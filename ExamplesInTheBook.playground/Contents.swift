5f4b6cf9ec9120b64ec538e4eb7d88dc9d7c73b4
AliGuled <ae7244xg@go.minneapolis.edu>
2019-01-28 13:12:51 -0600

    First commit

--- ExamplesInTheBook.playground/Contents.swift
+++ ExamplesInTheBook.playground/Contents.swift
@@ -2,7 +2,7 @@ import UIKit
 // string variable
 var str = "Hello, playground"
 
-//Part 1: Chapter 2, the Swift Language - Examples of the book.
+//*****Part 1: Chapter 2, the Swift Language - Examples of the book. **************
 
 var nextYear: Int = 2020 // integer variable
 var bodyTemp: Float = 96.0 //floating point variable
@@ -162,109 +162,3 @@ print(pieRawValvue)
 if let pieType = pieType(rawValue: pieRawValvue){
     print(pieType)
 }
-print()
-
-// Part A
-//Constants of prices and bus rides
-let regularFare: Float = 2.50 //The price of regular fare
-let rushFare: Float = 3.25 //The price of rush hour fare
-let numberOfTimesDuringRegular: Int = 4 //The number of times during regular rides
-let numberOfTimesDuringRush: Int = 8 //The number of times during rush hour
-
-
-//Calculating the total cost of bus rides
-let totalSpentRegularFare: Float = regularFare * Float(numberOfTimesDuringRegular)
-let totalspentRushFare: Float = rushFare * Float(numberOfTimesDuringRush)
-let totalSpentOnAllRides: Float = totalSpentRegularFare + totalspentRushFare
-
-
-//Printing out the calculated output of bus fares
-print("Total spent on regualr bus rides is $\(totalSpentRegularFare)")
-print("Total spent on rush hour fare is $\(totalspentRushFare)")
-print("Total spent on all rides is $\(totalSpentOnAllRides)")
-
-
-
-//Calculating the cost of regulare fare, using a funtion
-func costOfRegularFare(rides: Float) -> Float{
-    return regularFare * rides
-}
-//Calling the funtion and providing the number of rides
-let costRegularFare = costOfRegularFare(rides: 4)
-
-//Printing total cost of regular bus fare rides
-print("Total spent on regulare bus rides is $\(costRegularFare)")
-
-
-
-//Calculating the cost of rush hour fare, using a funtion
-func costOfRushFare(rides: Float) -> Float {
-    return rushFare * rides
-}
-let costRushFare = costOfRushFare(rides: 8)
-print("Total spent on rush hour fares is $\(costRushFare)")
-
-
-
-//Calculating the cost of total bus rides, using a funtion
-func totalCostBusRides(regulare: Float, rush: Float) -> Float{
-    return regulare + rush
-}
-
-//Calling the funtion and providing the number of rides and print the result
-let costTotalBusRides = totalCostBusRides(regulare: costRegularFare, rush: costRushFare)
-print("Total bus rides is $\(costTotalBusRides)")
-
-print()//space between my outputs
-
-//An array that stores the names of ITEC classes
-var namesOfItecClasses = ["Programming Logic and Design",
-                          "Apple Mobile",
-                          "Web Client and Server Programming"]
-
-//Adding another ITEC class to the array
-namesOfItecClasses += ["C# Programming"]
-print(namesOfItecClasses)
-print("Number of classes: \(namesOfItecClasses.count)")
-
-print()//space between my outputs
-
-//Filtering classes with 20 characters long
-var maxNumberOfCharacters = namesOfItecClasses.filter({$0.count > 20})
-print("Class names with 20 characters long: \(maxNumberOfCharacters)")
-
-print()//space between my outputs
-
-//Creating dictionary where the elements have 20 characters long and warning classes with 20 characters long
-for (index, classes) in namesOfItecClasses.enumerated(){
-    print("Index:\(index):\tClass:\(classes), Lenght:\(classes.count) characters")
-    
-    if classes.count > 20 {
-        print("Warning: class name must be less than 20 characters.\n")
-    } else{
-        print("Okay: class name is under 20 characters\n")
-    }
-}
-
-
-//Part C
-//Creating an array of more ITEC classes
-var moreITECClasses = ["itec", "BTEc", "maTh"]
-//Mapping the array as uppercased letters
-var properUpperCased = moreITECClasses.map({$0.uppercased()})
-print(properUpperCased)
-
-
-
-//looping all of the departments with closed range operator to print all departments
-for departs in 0..<properUpperCased.count{
-    
-    let allClasses = properUpperCased[departs]
-    print(allClasses)
-}
-//Adding more classes to the array and printing
-properUpperCased.append("BIO")
-print("Departments: \(properUpperCased)")
-
-
-
